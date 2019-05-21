40ccf38c020ac1be8d39f27b006bef509becb2fd
YannPoncet <yann.poncetpro@gmail.com>
2019-05-21 14:17:12 +0200

    Renaming functions

--- src/tools/meshvertex.cpp
+++ src/tools/meshvertex.cpp
@@ -9,7 +9,7 @@ void MeshVertex::computeNormalVector() {
 
 }
 
-void MeshVertex::axisDeformation(QVector3D axis) {
+void MeshVertex::axisDeform(QVector3D axis) {
     QMatrix4x4 deformation = QMatrix4x4(axis.x(),   0,          0,          0,
                                         0,          axis.y(),   0,          0,
                                         0,          0,          axis.z(),   0,
@@ -17,12 +17,12 @@ void MeshVertex::axisDeformation(QVector3D axis) {
     this->position = deformation*this->position;
 }
 
-void MeshVertex::translation(QVector3D axis) {
+void MeshVertex::translate(QVector3D axis) {
     this->position += axis;
 }
 
 // rotation of angle degrees with an axis and going through a point
-void MeshVertex::rotation(float angle, QVector3D axis, QVector3D point) {
+void MeshVertex::rotate(float angle, QVector3D axis, QVector3D point) {
     QQuaternion rotationQuat = QQuaternion::fromAxisAndAngle(axis,angle);
 
     this->position -= point;

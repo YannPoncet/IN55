96b85ab04504c57ea8366d84cdb96797aac39c72
Nafer17 <liliandrd@hotmail.fr>
2019-05-29 00:01:11 +0200

    Light fix

--- ressources/fshader.glsl
+++ ressources/fshader.glsl
@@ -4,6 +4,8 @@ in vec3 fColor;
 in vec3 fPosition;
 in vec3 fNormal;
 
+uniform mat4 mv;
+
 out vec4 fragColor;
 
 vec3 normalizeNormal, normalizeEyeCoord, normalizeLightVec, V, R, ambient, diffuse, specular;
@@ -11,7 +13,7 @@ float sIntensity, cosAngle;
 
 vec3 PhongShading()
 {
-    vec3 LightPosition = vec3(0,0,0);
+    vec3 LightPosition = vec3(mv*vec4(0,0,5,1));
     vec3 MaterialAmbient = fColor;
     vec3 LightAmbient = vec3(0.1,0.1,0.1);
     vec3 MaterialDiffuse = fColor;
@@ -21,7 +23,7 @@ vec3 PhongShading()
 
     normalizeNormal = normalize(fNormal);
     normalizeEyeCoord = normalize(fPosition);
-    normalizeLightVec = normalize(LightPosition-fPosition);
+    normalizeLightVec = normalize(LightPosition - fPosition);
 
     // Diffuse Intensity
     cosAngle = max(0.0,

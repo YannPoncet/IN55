59ed1d04a516d4649ab2fd438e569cbd8efadf37
Nafer17 <liliandrd@hotmail.fr>
2019-05-28 23:50:03 +0200

    adding ligh (Phong) on (0,0,0) camera space

--- ressources/fshader.glsl
+++ ressources/fshader.glsl
@@ -1,10 +1,43 @@
 #version 330
 
 in vec3 fColor;
+in vec3 fPosition;
+in vec3 fNormal;
 
 out vec4 fragColor;
 
+vec3 normalizeNormal, normalizeEyeCoord, normalizeLightVec, V, R, ambient, diffuse, specular;
+float sIntensity, cosAngle;
+
+vec3 PhongShading()
+{
+    vec3 LightPosition = vec3(0,0,0);
+    vec3 MaterialAmbient = fColor;
+    vec3 LightAmbient = vec3(0.1,0.1,0.1);
+    vec3 MaterialDiffuse = fColor;
+    vec3 LightDiffuse = vec3(1.0,1.0,1.0);
+    vec3 MaterialSpecular = fColor;
+    vec3 LightSpecular = vec3(1.0,1.0,1.0);
+
+    normalizeNormal = normalize(fNormal);
+    normalizeEyeCoord = normalize(fPosition);
+    normalizeLightVec = normalize(LightPosition-fPosition);
+
+    // Diffuse Intensity
+    cosAngle = max(0.0,
+    dot(normalizeNormal,normalizeLightVec));
+    // Specular Intensity
+    V = -normalizeEyeCoord; // Viewer's vector
+    R = reflect(-normalizeLightVec, normalizeNormal);
+    sIntensity = pow(max(0.0,dot(R,V)), 0);
+    ambient = MaterialAmbient * LightAmbient;
+    diffuse = MaterialDiffuse * LightDiffuse;
+    specular = MaterialSpecular * LightSpecular;
+
+    return ambient+(cosAngle*diffuse)+(sIntensity*specular);
+}
+
 void main()
 {
-    fragColor = vec4( fColor, 1.0 );
+    fragColor = vec4(PhongShading(), 1.0 );
 }
--- ressources/vshader.glsl
+++ ressources/vshader.glsl
@@ -8,50 +8,16 @@ in vec3 position;
 in vec3 color;
 in vec3 normal;
 
+out vec3 fNormal;
+out vec3 fPosition;
 out vec3 fColor;
 
 void main()
 {
-    // Vshader before Nafer's work
     // Calculate vertex position in screen space
     gl_Position = mvp * vec4( position, 1.0 );
+
+    fNormal = mat3(nm)*normal;
+    fPosition = vec3(mv*vec4(position,1.0));
     fColor = color;
-
-
-    /*
-    // Calculate vertex position in screen space
-    gl_Position = mvp * vec4(position, 1.0 );
-
-    //CM08a-44
-    vec3 sp = vec3(mv*vec4(position, 1.0));
-    vec3 V = -normalize(sp);
-    vec3 unit_normal = normalize(vec3(nm*vec4(normal, 1.0)));
-
-    float ambientStrength = 0.1;
-    vec3 lightPos = vec3(0, 0, -5);
-    vec3 lightColor = vec3(1.0f,1.0f,1.0f);
-    float constantAttenuation = 1.0;
-    float linearAttenuation = 0.0;
-    float quadraticAttenuation = 0.1;
-
-    //CM08a-46
-    vec3 L = lightPos - sp;
-    float d = length(L);
-    float attenuation = 1.0 / (constantAttenuation + linearAttenuation * d + quadraticAttenuation * d * d);
-
-    L = normalize(L);
-    vec3 halfway_vector = normalize(L+V);
-    float pf = 0.0; //power factor
-    float nDotH;
-
-    float nDotL = max(0.0, dot(unit_normal, L));
-    if(nDotL==0)
-        pf = 0.0;
-    else{
-        nDotH = max(0.0, dot(unit_normal, halfway_vector));
-        pf = pow(nDotH, 0.1);
-    }
-
-    fColor = (nDotL*attenuation+ambientStrength+pf*attenuation)*lightColor*color;
-    */
 }
